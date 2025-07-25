75a305de1d08c7a95fafea77e84eb797be9fee3d
EmilynnJ <emilynnj14@gmail.com>
2025-07-24 22:19:40 -0400

    kk

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,8 @@ import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
 
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+console.log('Clerk Key:', clerkPubKey);
+console.log('All env vars:', import.meta.env);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
